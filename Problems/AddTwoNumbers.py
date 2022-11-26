283a393626327738e7d755ffb39fed373c120716
Marko Bachvarovski <marko.bachvarovski@mail.utoronto.ca>
2022-11-26 00:19:28 -0500

    Added comments for AddTwoNumbers.py

--- Problems/AddTwoNumbers.py
+++ Problems/AddTwoNumbers.py
@@ -11,13 +11,23 @@ class Solution:
 
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
+        # Worst case runtime: O(n) where n is the number of nodes of the larger list
+
         answer = ListNode(None, None)
         remainder = 0
         head = answer
 
+
+        # Loop until you reach the end of both lists
         while not (l1 is None and l2 is None):
 
+            # If l2 is longer than l1
             if l1 is None:
+
+                # Can't simply connect l2 to answer, since there might be an unaccounted for remainder left over
+                # What we need to do is loop over l2 until the remainder is 0, and at that point it's safe to set
+                # answer.next = l2.next because we know we wouldn't have missed any leftover remainders
+                # Consider the case l1 = [9, 9, 9, 9, 9, 9, 9], l2 = [9, 9, 9, 9] to understand the purpose of this loop
                 while remainder != 0 or l2 is None:
                     if l2.val + remainder >= 10:
                         answer.val = (l2.val + remainder) % 10
@@ -26,7 +36,10 @@ class Solution:
                         answer.val = l2.val + remainder
                         remainder = 0
 
+                    # Checks ahead if the current node is the last one in l2 and adjusts answer accordingly
                     if l2.next is None:
+                        # If the current node is the last, but we have a remainder, then the actual answer
+                        # needs to have two more nodes to account for the remainder as well
                         if remainder != 0:
                             answer.next = ListNode(None, None)
                             answer.next.val = remainder
@@ -48,6 +61,7 @@ class Solution:
 
                 return head
 
+            # Same logic as above applies
             elif l2 is None:
                 while remainder != 0 or l1 is None:
                     if l1.val + remainder >= 10:
@@ -79,6 +93,7 @@ class Solution:
 
                 return head
 
+            # Same logic applies, except this time we're handling the values from both l1 and l2
             else:
 
                 if l1.val + l2.val + remainder >= 10:
@@ -109,14 +124,23 @@ class Solution:
 
 
 if __name__ == '__main__':
-    l1 = ListNode(2, ListNode(4, ListNode(3, None)))
-    l2 = ListNode(5, ListNode(6, ListNode(4, None)))
+    # Test case 1
+    # l1 = ListNode(2, ListNode(4, ListNode(3, None)))
+    # l2 = ListNode(5, ListNode(6, ListNode(4, None)))
 
-    # l1 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(8, ListNode(9, ListNode(9, None)))))))
-    # l2 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, None))))
-    #
+    # Test case 2
+    l1 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, None)))))))
+    l2 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, None))))
+
+    # Test case 3
     # l1 = ListNode(0, None)
     # l2 = ListNode(0, None)
 
     ans = Solution().addTwoNumbers(l1, l2)
-    print("Hi")
+
+    while ans is not None:
+        if ans.next is None:
+            print(ans.val)
+        else:
+            print(ans.val, end=" -> ")
+        ans = ans.next
