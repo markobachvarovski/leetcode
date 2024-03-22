04bbfed3bbfa2395a8a8f0a31d62b1d6eb5464df
Marko Bachvarovski <marko.bachvarovski@gmail.com>
2024-03-21 21:45:03 -0400

    Adding partial Python solution for Problem #88 - Merge Sorted Array

---
+++ PythonSolutions/Problem88-MergeSortedArray.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> List[int]:
+
+        index1 = m - 1
+        index2 = n - 1
+
+        if m == 0:
+            for i in range(0, n):
+                nums1[i] = nums2[i]
+        else:
+            for i in range(m+n-1, 0, -1):
+                if nums1[index1] >= nums2[index2]:
+                    nums1[i] = nums1[index1]
+                    index1 -= 1
+                else:
+                    nums1[i] = nums2[index2]
+                    index2 -= 1
+
+            if index2 != 0 and index1 == 0:
+                nums1[0] = nums2[0]
+
+        return nums1
+
+if __name__ == '__main__':
+    testStrings = {
+        1: [[1,2,3,0,0,0], 3, [2,5,6], 3],
+        2: [[1], 1, [], 0],
+        3: [[0], 0, [1], 1],
+        4: [[2,0], 1, [1], 1]
+    }
+    key = 4
+    Solution().merge(testStrings[key][0], testStrings[key][1], testStrings[key][2], testStrings[key][3])
+    for key in testStrings:
+        print(f"The merged result of {testStrings[key][0]} and {testStrings[key][2]} is: {Solution().merge(testStrings[key][0], testStrings[key][1], testStrings[key][2], testStrings[key][3])}")
