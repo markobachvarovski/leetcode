28601cd06a8058fda967855a44c816b022237d48
Marko Bachvarovski <marko.bachvarovski@gmail.com>
2024-07-01 22:36:02 -0400

    Adding solution for Problem 138 - Copy List With Random Pointer

---
+++ PythonSolutions/Problem138-CopyListWithRandomPointer.py
@@ -0,0 +1,55 @@
+# Definition for a Node.
+from typing import Optional
+
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def copyRandomList(self, head: Optional[Node]) -> Optional[Node]:
+        if not head:
+            return None
+
+        curr = head
+        while curr:
+            new_node = Node(curr.val, curr.next)
+            curr.next = new_node
+            curr = new_node.next
+
+        curr = head
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+
+        old_head = head
+        new_head = head.next
+        curr_old = old_head
+        curr_new = new_head
+
+        while curr_old:
+            curr_old.next = curr_old.next.next
+            curr_new.next = curr_new.next.next if curr_new.next else None
+            curr_old = curr_old.next
+            curr_new = curr_new.next
+
+        return new_head
+
+
+if __name__ == '__main__':
+    testStrings = {
+        1: ["a", "b"],
+        2: ["aa", "ab"],
+        3: ["aa", "aab"],
+        4: ["bg", "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj"]
+    }
+
+    for key in testStrings:
+        if Solution().canConstruct(testStrings[key][0], testStrings[key][1]):
+            print(f"{testStrings[key][0]} can be constructed from the letters in {testStrings[key][1]}")
+        else:
+            print(f"{testStrings[key][0]} can not be constructed from the letters in {testStrings[key][1]}")
