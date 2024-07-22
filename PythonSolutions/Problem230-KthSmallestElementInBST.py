fecc891b7866487497106fbf41c0de7e3cf1a894
Marko Bachvarovski <marko.bachvarovski@gmail.com>
2024-07-21 21:45:32 -0400

    Modifying solution for Problem 230 - Kth Smallest Element in BST

--- PythonSolutions/Problem230-KthSmallestElementInBST.py
+++ PythonSolutions/Problem230-KthSmallestElementInBST.py
@@ -23,29 +23,35 @@ def to_binary_tree(items):
     return root
 class Solution(object):
     def kthSmallest(self, root, k):
-        self.count = 0
-        self.result = 0
-        self.inorderTraversal(root, k)
-        return self.result
+        count = 0
 
-    def inorderTraversal(self, node, k):
-        if not node or self.count >= k:
-            return
+        def inorderTraversal(node, k):
+            nonlocal count
 
-        self.inorderTraversal(node.left, k)
+            if not node or count > k:
+                return
 
-        self.count += 1
-        if self.count == k:
-            self.result = node.val
-            return
+            left = inorderTraversal(node.left, k)
+            if left is not None:
+                return left
 
-        self.inorderTraversal(node.right, k)
+            count += 1
+            if count == k:
+                return node.val
+
+            return inorderTraversal(node.right, k)
+
+            # return left or right
+
+        return inorderTraversal(root, k)
+
 
 if __name__ == '__main__':
     testCases = {
         1: [[3, 1, 4, None, 2], 1],
         2: [[5, 3, 6, 2, 4, None, None, 1], 3],
-        3: [[100, 80, 120, 70, 90, 110, 130, 60, None, 85, 95, None, None, None, 140, None, None, None, None, 94, None, None, None], 6]
+        3: [[100, 80, 120, 70, 90, 110, 130, 60, None, 85, 95, None, None, None, 140, None, None, None, None, 94, None, None, None], 6],
+        4: [[31,30,48,3,None,38,49,0,16,35,47,None,None,None,2,15,27,33,37,39,None,1,None,5,None,22,28,32,34,36,None,None,43,None,None,4,11,19,23,None,29,None,None,None,None,None,None,40,46,None,None,7,14,17,21,None,26,None,None,None,41,44,None,6,10,13,None,None,18,20,None,25,None,None,42,None,45,None,None,8,None,12,None,None,None,None,None,24,None,None,None,None,None,None,9], 1]
     }
 
     for key in testCases:
