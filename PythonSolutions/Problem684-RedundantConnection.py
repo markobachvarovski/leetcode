af483a9462f13e4c373f40314b035f846b670d18
Marko Bachvarovski <marko.bachvarovski@gmail.com>
2024-09-03 21:58:30 -0400

    Adding solution for Problem 684 - Redundant Connection

---
+++ PythonSolutions/Problem684-RedundantConnection.py
@@ -0,0 +1,42 @@
+from collections import deque
+from typing import Optional, List, Set
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        par = [i for i in range(len(edges) + 1)]
+        rank = [1] * (len(edges) + 1)
+
+        def find(n):
+            p = par[n]
+            while p != par[p]:
+                par[p] = par[par[p]]
+                p = par[p]
+            return p
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return [n1, n2]
+
+
+if __name__ == '__main__':
+    testCases = {
+        1: [[1,2],[1,3],[2,3]],
+        2: [[1,2],[2,3],[3,4],[1,4],[1,5]],
+    }
+
+    for key in testCases:
+        print(Solution().findRedundantConnection(testCases[key]))
